15ff52817e929287b530391a3f256ab5d4855cb1
IulianBotnari <iulianbotnari93@gmail.com>
2025-02-20 12:54:19 +0100

    [WIP] Smartphone class done

--- src/org/shop/App.java
+++ src/org/shop/App.java
@@ -1,9 +1,14 @@
 package org.shop;
 
+import java.math.BigDecimal;
+
 public class App {
 
     public static void main(String[] args) {
         
+        Smartphone SmartphoneNumero1 = new Smartphone(500, "ROG", "Asus", BigDecimal.valueOf(1000), (byte)800, "4964646546456496", "256 GB");
+        System.out.println(SmartphoneNumero1.getSmartphoneInfo());
     }
+
     
 }
--- src/org/shop/Smartphone.java
+++ src/org/shop/Smartphone.java
@@ -1,5 +1,59 @@
 package org.shop;
 
-public class Smartphone {
+import java.math.BigDecimal;
+
+public class Smartphone extends Product {
+
+    protected String IMEI;
+    protected String memorySize;
+
+    public Smartphone(int code, String name, String brand, BigDecimal price, byte iva, String IMEI, String memorySize){
+        super(code, name, brand, price, iva);
+
+    this.IMEI = IMEI;
+    this.memorySize = memorySize;
+    }
+
+    // get smartphone methods
+
+
+    public String getImei(){
+        return this.IMEI;
+    }
+
+    public String getMemorySize(){
+        return  this.memorySize;
+    }
+
+
+    public String getSmartphoneInfo(){
+        return 
+        "Info Smartphone: " +
+        "\nCodice: " + this.getProductCode() +
+        "\nNome prodotto: " + this.name +
+        "\nBrand prodotto: " + this.brand + 
+        "\nIMEI: " + this.IMEI + 
+        "\nMemoria: " + this.memorySize +
+        "\nPrezzo: " + this.price + 
+        "\nIva: " + this.iva;
+    }
+
+    
+
+
+    // set smartphone methods
+
+    public void setImei(String IMEI){
+        this.IMEI = IMEI;
+    }
+
+    public void setMemorySize(String memorySize){
+        this.memorySize = memorySize;
+    }
+
+
+
+
+
     
 }
