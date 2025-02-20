c924503a79bb88fe7d4d759039504552ae74f599
IulianBotnari <iulianbotnari93@gmail.com>
2025-02-20 12:06:32 +0100

    [WIP] Product classe done

--- src/org/shop/Product.java
+++ src/org/shop/Product.java
@@ -1,13 +1,71 @@
 package org.shop;
 
+import java.math.BigDecimal;
+
 public class Product {
 
     private int code;
     protected String name;
     protected String brand;
-    protected Bigdecimal prezzo;
+    protected BigDecimal price;
     protected byte iva;
-    
 
-    
+    public Product(int code, String name, String brand, BigDecimal price, byte iva) {
+        this.code = code;
+        this.name = name;
+        this.brand = brand;
+        this.price = price;
+        this.iva = iva;
+
+    }
+
+    // set methods
+
+    public void setProductName(String name) {
+        this.name = name;
+    }
+
+    public void setProductBrand(String brand) {
+        this.brand = brand;
+    }
+
+    public void setProductprice(BigDecimal price) {
+        if (price.compareTo(BigDecimal.ZERO) >= 0) {
+            this.price = price;
+        } else {
+            System.out.println("Il valore inserito non è valido");
+        }
+    }
+
+    public void setProductIva(byte iva) {
+        if (iva >= 0) {
+            this.iva = iva;
+
+        } else {
+            System.out.println("Il valore inserito non è valido");
+        }
+    }
+
+    // get methods
+
+    public int getProductCode() {
+        return this.code;
+    }
+
+    public String getProductName() {
+        return this.name;
+    }
+
+    public String getProductBrand() {
+        return this.brand;
+    }
+
+    public BigDecimal getProductPrice() {
+        return this.price;
+    }
+
+    public byte getProductIva() {
+        return this.iva;
+    }
+
 }
