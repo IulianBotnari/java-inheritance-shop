669ab4b9ff7dab4fe0cc5921f8d9e1d6caddc627
IulianBotnari <iulianbotnari93@gmail.com>
2025-02-20 10:13:50 +0100

    first commit

---
+++ src/org/shop/App.java
@@ -0,0 +1,9 @@
+package org.shop;
+
+public class App {
+
+    public static void main(String[] args) {
+        
+    }
+    
+}
---
+++ src/org/shop/Product.java
@@ -0,0 +1,13 @@
+package org.shop;
+
+public class Product {
+
+    private int code;
+    protected String name;
+    protected String brand;
+    protected Bigdecimal prezzo;
+    protected byte iva;
+    
+
+    
+}
