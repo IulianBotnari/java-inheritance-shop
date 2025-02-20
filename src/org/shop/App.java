4a042d9d720811766e6920901fa577a04c409bbe
IulianBotnari <iulianbotnari93@gmail.com>
2025-02-20 14:15:49 +0100

    [WIP] add tv class

--- src/org/shop/App.java
+++ src/org/shop/App.java
@@ -8,6 +8,12 @@ public class App {
         
         Smartphone SmartphoneNumero1 = new Smartphone(500, "ROG", "Asus", BigDecimal.valueOf(1000), (byte)800, "4964646546456496", "256 GB");
         System.out.println(SmartphoneNumero1.getSmartphoneInfo());
+
+        System.out.println();
+        Tv Tvnumero1 = new Tv(600, "SmartTV", "Samsung", BigDecimal.valueOf(1000), (byte) 22, 200, 100, true);
+        Tvnumero1.setTvHeigth(100.32f);
+        Tvnumero1.setTvWidth(200.56f);
+        System.out.println(Tvnumero1.getTvInfo());
     }
 
     
--- src/org/shop/Tv.java
+++ src/org/shop/Tv.java
@@ -1,5 +1,63 @@
 package org.shop;
 
-public class Tv {
+import java.math.BigDecimal;
+import java.text.DecimalFormat;
+
+public class Tv extends Product{
+
+    DecimalFormat formattaNumeriDecimali = new DecimalFormat("0.##");
+
+    protected float width;
+    protected float heigth;
+    protected boolean smart;
+
+
+    public Tv(int code, String name, String brand, BigDecimal price, byte iva, float width, float heigth, boolean smart){
+        super(code, name, brand, price, iva);
+
+        this.width = width;
+        this.heigth = heigth;
+        this.smart = smart;
+    }
+    // get tv methods
+    public String getTvInfo(){
+        return 
+        "Info TV: " +
+        "\nCodice: " + this.getProductCode() +
+        "\nNome prodotto: " + this.name +
+        "\nBrand prodotto: " + this.brand + 
+        "\nLarghezza: " + formattaNumeriDecimali.format(this.width) + 
+        "\nAltezza: " + formattaNumeriDecimali.format(this.heigth) +
+        "\nSmart: " + this.smart +
+        "\nPrezzo: " + this.price + 
+        "\nIva: " + this.iva;
+    }
+
+    public float getTvWidth(){
+        return this.width;
+    }
+
+    public float getTvHeigth(){
+        return this.heigth;
+    }
+
+    public boolean getTvSmart(){
+        return this.smart;
+    }
+
+
+    // set tv methods
+    public void setTvWidth(float width){
+        this.width = width;
+    }
+
+    public void setTvHeigth(float heigth){
+        this.heigth = heigth;
+    }
+
+    public void setTvSmart(boolean smart){
+        this.smart = smart;
+    }
+
     
 }
